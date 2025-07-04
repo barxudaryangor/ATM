057bcbb7109870887e99e7471412a45659136ebc
barxudaryagor <gor.barxudaryan.2014@gmail.com>
2025-07-04 17:47:12 +0400

    Adding docker container

--- src/test/java/org/example/atm/BankCreationTest.java
+++ src/test/java/org/example/atm/BankCreationTest.java
@@ -5,13 +5,20 @@ import org.example.atm.controllers.BankController;
 import org.example.atm.dtos.BankDTO;
 import org.example.atm.services.BankServiceImpl;
 import org.example.atm.short_dtos.BankAccountShortDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
@@ -20,17 +27,36 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(BankController.class)
+@SpringBootTest
+@Testcontainers
+
 
 public class BankCreationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Autowired
-    MockMvc mockMvc;
+    WebApplicationContext context;
+
+    private MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    BankServiceImpl bankService;
+    @BeforeEach
+    void setUpMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
     void testCreateBank() throws Exception {
@@ -39,33 +65,27 @@ public class BankCreationTest {
                 "AraratBank",
                 "Erevan",
                 List.of(new BankAccountShortDTO(
-                        1L, "GB220205", 1000.0
-                ))
-        );
-
-        BankDTO response = new BankDTO(
-                1L,
-                "AraratBank",
-                "Erevan",
-                List.of(new BankAccountShortDTO(
-                        1L, "GB220205", 1000.0
+                        null, "GB220205", 1000.0
                 ))
         );
 
         String requestBody = objectMapper.writeValueAsString(request);
-        Mockito.when(bankService.createBank(any(BankDTO.class))).thenReturn(response);
 
         mockMvc.perform(post("/atm/banks")
-                    .content(requestBody)
-                    .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(result -> {
+                    System.out.println("==== RESPONSE BODY ====");
+                    System.out.println(result.getResponse().getContentAsString());
+                    System.out.println("=======================");
+                })
+
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.name").value("AraratBank"))
                 .andExpect(jsonPath("$.location").value("Erevan"))
-                .andExpect(jsonPath("$.bankAccounts[0].id").value(1))
+                .andExpect(jsonPath("$.bankAccounts[0].id").isNumber())
                 .andExpect(jsonPath("$.bankAccounts[0].account_num").value("GB220205"))
                 .andExpect(jsonPath("$.bankAccounts[0].balance").value(1000.0));
-
-
     }
 }
